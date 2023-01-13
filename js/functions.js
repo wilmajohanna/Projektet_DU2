7b6b0a1bdb09d0f847cf07539556c5bc4d986414
Wilma Gotthardsson <93529076+wilmajohanna@users.noreply.github.com>
2023-01-13 13:19:50 +0100

    fixat funktionen

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,9 @@
 
 // G, CODE According to specification
 function click_filter_element (event) {
+  const filterElement = event.target;
+  filterElement.classList.toggle('selected');
+  update_programmes();
 
 }
 
